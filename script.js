4a75eca0d44b9414bdd4722ccc9fd91ff1b9e398
Diego Oliveira Pereira <dpereir@icloud.com>
2026-05-17 20:52:40 -0300

    feat: Implementa accordion para detalhamento de experiência técnica

--- script.js
+++ script.js
@@ -2,36 +2,47 @@
 
 const linksInterno = document.querySelectorAll('a[href^="#"]');
 
-function scrollSuave(event){
-    event.preventDefault();
-    const href = event.currentTarget.getAttribute('href');
-    const section = document.querySelector(href);
-    section.scrollIntoView({
-        behavior: 'smooth',
-        block: 'start',
-    })
-};
+function scrollSuave(event) {
+  event.preventDefault();
+  const href = event.currentTarget.getAttribute("href");
+  const section = document.querySelector(href);
+  section.scrollIntoView({
+    behavior: "smooth",
+    block: "start",
+  });
+}
 
-linksInterno.forEach((item)=>{
-    item.addEventListener('click', scrollSuave);
+linksInterno.forEach((item) => {
+  item.addEventListener("click", scrollSuave);
 });
 
 // Animação do site Slide
 
 const elementosSection = document.querySelectorAll("section *");
-elementosSection.forEach((item)=>{
-item.classList.add('animacao');
-})
+elementosSection.forEach((item) => {
+  item.classList.add("animacao");
+});
 
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add("animacao--ativo");
-      observer.unobserve(entry.target); 
-    }
-}) 
-}, {threshold: 0.2}); 
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("animacao--ativo");
+        observer.unobserve(entry.target);
+      }
+    });
+  },
+  { threshold: 0.2 },
+);
 
 elementosSection.forEach((item) => {
   observer.observe(item);
+});
+
+const saibaMais = document.querySelector(".saibaMais");
+const accordion = document.querySelector(".accordion");
+
+saibaMais.addEventListener("click", (event) => {
+  event.preventDefault();
+  accordion.classList.toggle("ativo");
 });
